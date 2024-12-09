a0c16a892f60411639ccb81f7feeab08f9c8d606
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2024-12-09 10:36:40 +0100

    Install sudo in /usr/local on FreeBSD in tests

--- test-framework/sudo-compliance-tests/src/sudo/flag_list.rs
+++ test-framework/sudo-compliance-tests/src/sudo/flag_list.rs
@@ -1,4 +1,4 @@
-use sudo_test::{Command, Env, TextFile, User, BIN_FALSE, BIN_LS, BIN_PWD, BIN_TRUE};
+use sudo_test::{Command, Env, TextFile, User, BIN_FALSE, BIN_LS, BIN_PWD, BIN_SUDO, BIN_TRUE};
 
 use crate::{Result, PANIC_EXIT_CODE, PASSWORD, SUDOERS_ALL_ALL_NOPASSWD, USERNAME};
 
@@ -431,7 +431,7 @@ fn resolves_command_in_invoking_users_path_pass() -> Result<()> {
         .build()?;
 
     let output = Command::new("env")
-        .args(["-i", "PATH=/tmp", "/usr/bin/sudo", "-l", "true"])
+        .args(["-i", "PATH=/tmp", BIN_SUDO, "-l", "true"])
         .output(&env)?;
 
     let actual = output.stdout()?;
