f56746118674ca9b2dab0938573fdf6441fef45d
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2025-04-14 16:10:57 +0200

    Use seccomp_unotify

--- sudo_noexec_so/src/lib.rs
+++ sudo_noexec_so/src/lib.rs
@@ -1,35 +1,27 @@
-/*
- * SPDX-License-Identifier: ISC
- *
- * Copyright (c) 2004-2005, 2010-2018 Todd C. Miller <Todd.Miller@sudo.ws>
- *
- * Permission to use, copy, modify, and distribute this software for any
- * purpose with or without fee is hereby granted, provided that the above
- * copyright notice and this permission notice appear in all copies.
- *
- * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
- * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
- * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
- * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
- * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
- */
-
 // On Linux we can use a seccomp() filter to disable exec.
 
 #[cfg(not(target_os = "linux"))]
 compile_error!("sudo_noexec shouldn't be compiled for non-Linux systems");
 
+use std::cmp;
 use std::mem::offset_of;
 use std::ptr::addr_of;
 
 use libc::{
-    BPF_ABS, BPF_JEQ, BPF_JMP, BPF_JUMP, BPF_K, BPF_LD, BPF_RET, BPF_STMT, EACCES,
-    PR_SET_NO_NEW_PRIVS, PR_SET_SECCOMP, SECCOMP_MODE_FILTER, SECCOMP_RET_ALLOW, SECCOMP_RET_DATA,
-    SECCOMP_RET_ERRNO, SYS_execve, SYS_execveat, c_uint, prctl, seccomp_data, sock_filter,
-    sock_fprog,
+    BPF_ABS, BPF_JEQ, BPF_JMP, BPF_JUMP, BPF_K, BPF_LD, BPF_RET, BPF_STMT, PR_SET_NO_NEW_PRIVS,
+    SECCOMP_FILTER_FLAG_NEW_LISTENER, SECCOMP_GET_NOTIF_SIZES, SECCOMP_RET_ALLOW,
+    SECCOMP_SET_MODE_FILTER, SECCOMP_USER_NOTIF_FLAG_CONTINUE, SYS_execve, SYS_execveat,
+    SYS_seccomp, c_int, c_uint, c_ulong, calloc, close, fork, ioctl, prctl, seccomp_data,
+    seccomp_notif, seccomp_notif_resp, seccomp_notif_sizes, sock_filter, sock_fprog, syscall,
 };
+
+const SECCOMP_RET_USER_NOTIF: c_uint = 0x7fc00000;
+const SECCOMP_IOCTL_NOTIF_RECV: c_ulong = 0xc0502100;
+const SECCOMP_IOCTL_NOTIF_SEND: c_ulong = 0xc0182101;
+
+unsafe fn seccomp<T>(operation: c_uint, flags: c_uint, args: *mut T) -> c_int {
+    unsafe { syscall(SYS_seccomp, operation, flags, args) as c_int }
+}
 
 #[used]
 #[unsafe(link_section = ".init_array")]
@@ -41,16 +33,13 @@ extern "C" fn noexec_ctor() {
         [
             // Load syscall number into the accumulator
             BPF_STMT((BPF_LD | BPF_ABS) as _, offset_of!(seccomp_data, nr) as _),
-            // Jump to deny for execve/execveat
+            // Jump to user notify for execve/execveat
             BPF_JUMP((BPF_JMP | BPF_JEQ | BPF_K) as _, SYS_execve as _, 2, 0),
             BPF_JUMP((BPF_JMP | BPF_JEQ | BPF_K) as _, SYS_execveat as _, 1, 0),
             // Allow non-matching syscalls
             BPF_STMT((BPF_RET | BPF_K) as _, SECCOMP_RET_ALLOW),
-            // Deny execve/execveat syscall
-            BPF_STMT(
-                (BPF_RET | BPF_K) as _,
-                (SECCOMP_RET_ERRNO | (EACCES as c_uint & SECCOMP_RET_DATA)) as _,
-            ),
+            // Notify sudo about execve/execveat syscall
+            BPF_STMT((BPF_RET | BPF_K) as _, SECCOMP_RET_USER_NOTIF as _),
         ]
     };
 
@@ -62,10 +51,66 @@ extern "C" fn noexec_ctor() {
     // SAFETY: The first prctl is trivially safe as it doesn't touch any memory
     // and the second prctl passes a valid sock_fprog as argument.
     unsafe {
-        // SECCOMP_MODE_FILTER will fail unless the process has
+        // SECCOMP_SET_MODE_FILTER will fail unless the process has
         // CAP_SYS_ADMIN or the no_new_privs bit is set.
         if prctl(PR_SET_NO_NEW_PRIVS, 1, 0, 0, 0) == 0 {
-            prctl(PR_SET_SECCOMP, SECCOMP_MODE_FILTER, &exec_fprog);
+            // While the man page warns againt using seccomp_unotify as security
+            // mechanism, the TOCTOU problem that is described there isn't
+            // relevant here. We only SECCOMP_USER_NOTIF_FLAG_CONTINUE the first
+            // execve which is done by ourself and thus trusted.
+            // FIXME handle error
+            let notify_fd = seccomp(
+                SECCOMP_SET_MODE_FILTER,
+                SECCOMP_FILTER_FLAG_NEW_LISTENER as _,
+                &exec_fprog as *const sock_fprog as *mut sock_fprog,
+            );
+            if fork() == 0 {
+                close(notify_fd);
+            } else {
+                let mut sizes = seccomp_notif_sizes {
+                    seccomp_notif: 0,
+                    seccomp_notif_resp: 0,
+                    seccomp_data: 0,
+                };
+                if seccomp(SECCOMP_GET_NOTIF_SIZES, 0, &mut sizes) == -1 {
+                    libc::abort();
+                }
+
+                let req = calloc(
+                    1,
+                    cmp::max(sizes.seccomp_notif.into(), size_of::<seccomp_notif>()),
+                )
+                .cast::<seccomp_notif>();
+                let resp = calloc(
+                    1,
+                    cmp::max(
+                        sizes.seccomp_notif_resp.into(),
+                        size_of::<seccomp_notif_resp>(),
+                    ),
+                )
+                .cast::<seccomp_notif_resp>();
+                if req.is_null() || resp.is_null() {
+                    libc::abort();
+                }
+
+                // FIXME handle error
+                ioctl(notify_fd, SECCOMP_IOCTL_NOTIF_RECV, req);
+
+                (*resp).id = (*req).id;
+                (*resp).val = 0;
+                (*resp).error = 0;
+                (*resp).flags = SECCOMP_USER_NOTIF_FLAG_CONTINUE as u32;
+
+                // FIXME handle error
+                ioctl(notify_fd, SECCOMP_IOCTL_NOTIF_SEND, resp);
+
+                // Exit the helper process after the target process has been
+                // exec'ed. This will close the seccomp_unotify fd after which
+                // all SECCOMP_RET_USER_NOTIF will result in ENOSYS
+
+                // FIXME return EACCESS rather than ENOSYS.
+                libc::exit(0);
+            }
         }
     }
 }
