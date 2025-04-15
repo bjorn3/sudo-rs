147db725492ee6fc51bf558555520f76d675aba3
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2025-04-15 11:13:27 +0200

    Move to separate module

--- src/exec/mod.rs
+++ src/exec/mod.rs
@@ -1,6 +1,8 @@
 mod event;
 mod io_util;
 mod no_pty;
+#[cfg(target_os = "linux")]
+mod noexec;
 mod use_pty;
 
 use std::{
@@ -46,189 +48,6 @@ pub struct RunOptions<'a> {
     pub noexec: bool,
 }
 
-mod noexec {
-    // On Linux we can use a seccomp() filter to disable exec.
-
-    #[cfg(not(target_os = "linux"))]
-    compile_error!("sudo_noexec shouldn't be compiled for non-Linux systems");
-
-    use std::alloc::{handle_alloc_error, GlobalAlloc, Layout};
-    use std::mem::offset_of;
-    use std::os::unix::process::CommandExt;
-    use std::process::Command;
-    use std::ptr::addr_of;
-    use std::{cmp, io};
-
-    use libc::{
-        c_int, c_uint, c_ulong, close, fork, ioctl, prctl, seccomp_data, seccomp_notif,
-        seccomp_notif_resp, seccomp_notif_sizes, sock_filter, sock_fprog, syscall, SYS_execve,
-        SYS_execveat, SYS_seccomp, __errno_location, BPF_ABS, BPF_JEQ, BPF_JMP, BPF_JUMP, BPF_K,
-        BPF_LD, BPF_RET, BPF_STMT, EACCES, ENOENT, PR_SET_NO_NEW_PRIVS,
-        SECCOMP_FILTER_FLAG_NEW_LISTENER, SECCOMP_GET_NOTIF_SIZES, SECCOMP_RET_ALLOW,
-        SECCOMP_SET_MODE_FILTER,
-    };
-
-    const SECCOMP_RET_USER_NOTIF: c_uint = 0x7fc00000;
-    const SECCOMP_IOCTL_NOTIF_RECV: c_ulong = 0xc0502100;
-    const SECCOMP_IOCTL_NOTIF_SEND: c_ulong = 0xc0182101;
-
-    unsafe fn seccomp<T>(operation: c_uint, flags: c_uint, args: *mut T) -> c_int {
-        unsafe { syscall(SYS_seccomp, operation, flags, args) as c_int }
-    }
-
-    struct NotifyAllocs {
-        req: *mut seccomp_notif,
-        req_size: usize,
-        resp: *mut seccomp_notif_resp,
-    }
-
-    unsafe impl Send for NotifyAllocs {}
-    unsafe impl Sync for NotifyAllocs {}
-
-    fn alloc_dynamic<T>(runtime_size: u16) -> (*mut T, usize) {
-        const {
-            assert!(size_of::<T>() > 0);
-        }
-
-        let layout = Layout::from_size_align(
-            cmp::max(runtime_size.into(), size_of::<T>()),
-            align_of::<seccomp_notif>(),
-        )
-        .unwrap();
-
-        // SAFETY: We assert that T is bigger than 0 bytes and as such the computed layout is also
-        // bigger.
-        let ptr = unsafe { std::alloc::System.alloc_zeroed(layout).cast::<T>() };
-        if ptr.is_null() {
-            handle_alloc_error(layout);
-        }
-
-        (ptr, layout.size())
-    }
-
-    fn alloc_notify_allocs() -> NotifyAllocs {
-        let mut sizes = seccomp_notif_sizes {
-            seccomp_notif: 0,
-            seccomp_notif_resp: 0,
-            seccomp_data: 0,
-        };
-        // SAFETY: A valid seccomp_notif_sizes pointer is passed in
-        if unsafe { seccomp(SECCOMP_GET_NOTIF_SIZES, 0, &mut sizes) } == -1 {
-            panic!(
-                "failed to get sizes for seccomp unotify data structures: {}",
-                io::Error::last_os_error(),
-            );
-        }
-
-        let (req, req_size) = alloc_dynamic::<seccomp_notif>(sizes.seccomp_notif);
-        let (resp, _) = alloc_dynamic::<seccomp_notif_resp>(sizes.seccomp_notif_resp);
-
-        NotifyAllocs {
-            req,
-            req_size,
-            resp,
-        }
-    }
-
-    unsafe fn handle_notifications(
-        notify_fd: c_int,
-        NotifyAllocs {
-            req,
-            req_size,
-            resp,
-        }: NotifyAllocs,
-    ) -> ! {
-        unsafe {
-            loop {
-                // SECCOMP_IOCTL_NOTIF_RECV expects the target struct to be zeroed
-                // SAFETY: req is at least req_size bytes big
-                std::ptr::write_bytes(req.cast::<u8>(), 0, req_size);
-
-                // SAFETY: A valid pointer to a seccomp_notify is passed in
-                if ioctl(notify_fd, SECCOMP_IOCTL_NOTIF_RECV, req) == -1 {
-                    // SAFETY: Trivial
-                    if *__errno_location() == ENOENT {
-                        continue; // Syscall was interrupted
-                    }
-                    // SAFETY: Not actually unsafe
-                    libc::abort();
-                }
-
-                // Set the error code for all syscalls we are notified about to EACCESS.
-                // SAFETY: resp is a valid pointer to a seccomp_notify_resp
-                (*resp).id = (*req).id;
-                (*resp).val = 0;
-                (*resp).error = -EACCES;
-                (*resp).flags = 0;
-
-                if ioctl(notify_fd, SECCOMP_IOCTL_NOTIF_SEND, resp) == -1 {
-                    // SAFETY: Trivial
-                    if *__errno_location() == ENOENT {
-                        continue; // Syscall was interrupted
-                    }
-                    // SAFETY: Not actually unsafe
-                    libc::abort();
-                }
-            }
-        }
-    }
-
-    pub fn add_noexec_filter(command: &mut Command) {
-        let mut notify_allocs = Some(alloc_notify_allocs());
-
-        unsafe {
-            command.pre_exec(move || {
-                // SAFETY: libc unnecessarily marks these functions as unsafe
-                let exec_filter: [sock_filter; 5] = [
-                    // Load syscall number into the accumulator
-                    BPF_STMT((BPF_LD | BPF_ABS) as _, offset_of!(seccomp_data, nr) as _),
-                    // Jump to user notify for execve/execveat
-                    BPF_JUMP((BPF_JMP | BPF_JEQ | BPF_K) as _, SYS_execve as _, 2, 0),
-                    BPF_JUMP((BPF_JMP | BPF_JEQ | BPF_K) as _, SYS_execveat as _, 1, 0),
-                    // Allow non-matching syscalls
-                    BPF_STMT((BPF_RET | BPF_K) as _, SECCOMP_RET_ALLOW),
-                    // Notify sudo about execve/execveat syscall
-                    BPF_STMT((BPF_RET | BPF_K) as _, SECCOMP_RET_USER_NOTIF as _),
-                ];
-
-                let exec_fprog = sock_fprog {
-                    len: 5,
-                    filter: addr_of!(exec_filter) as *mut sock_filter,
-                };
-
-                // SAFETY: Trivially safe as it doesn't touch any memory.
-                // SECCOMP_SET_MODE_FILTER will fail unless the process has
-                // CAP_SYS_ADMIN or the no_new_privs bit is set.
-                if prctl(PR_SET_NO_NEW_PRIVS, 1, 0, 0, 0) == -1 {
-                    return Err(io::Error::last_os_error());
-                }
-
-                // While the man page warns againt using seccomp_unotify as security
-                // mechanism, the TOCTOU problem that is described there isn't
-                // relevant here. We only SECCOMP_USER_NOTIF_FLAG_CONTINUE the first
-                // execve which is done by ourself and thus trusted.
-                // FIXME handle error
-                // SAFETY: Passes a valid sock_fprog as argument.
-                let notify_fd = seccomp(
-                    SECCOMP_SET_MODE_FILTER,
-                    SECCOMP_FILTER_FLAG_NEW_LISTENER as _,
-                    addr_of!(exec_fprog).cast_mut(),
-                );
-
-                if fork() != 0 {
-                    close(notify_fd);
-                    return Ok(());
-                }
-
-                handle_notifications(
-                    notify_fd,
-                    notify_allocs.take().unwrap_or_else(|| libc::abort()),
-                );
-            });
-        }
-    }
-}
-
 /// Based on `ogsudo`s `exec_pty` function.
 ///
 /// Returns the [`ExitReason`] of the command and a function that restores the default handler for
@@ -259,7 +78,11 @@ pub fn run_command(
     }
 
     if options.noexec {
+        #[cfg(target_os = "linux")]
         noexec::add_noexec_filter(&mut command);
+
+        #[cfg(not(target_os = "linux"))]
+        return Err(io::Error::other("NOEXEC is currently only supported on Linux"))
     }
 
     // Decide if the pwd should be changed. `--chdir` takes precedence over `-i`.
